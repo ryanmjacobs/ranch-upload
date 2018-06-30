72c7c87cf8dec72db064a88f9bf82cb9c879fb17
Ryan Jacobs <ryan@rmj.us>
2018-06-30 17:04:48 -0600

    final fix?

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const chokidar = require("chokidar");
 
 const TMP_DIR = "tmp";
 //const FOOTAGE_DIRECTORY = "/tmp/samples";
-//const FOOTAGE_DIRECTORY = 'M:\MediaDatabase';
-const FOOTAGE_DIRECTORY = "../MediaDatabase";
+const FOOTAGE_DIRECTORY = "M:\\MediaDatabase";
 
 console.log("dependencies loaded.");
 
