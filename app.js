a915e5e4e13ad0f4f23ee79a3746480e259cb668
Ryan Jacobs <ryan@rmj.us>
2018-06-30 16:53:34 -0600

    cur dir

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const chokidar = require("chokidar");
 
 const TMP_DIR = "tmp";
 //const FOOTAGE_DIRECTORY = "/tmp/samples";
-const FOOTAGE_DIRECTORY = 'M:\MediaDatabase';
+//const FOOTAGE_DIRECTORY = 'M:\MediaDatabase';
+const FOOTAGE_DIRECTORY = ".";
 
 console.log("dependencies loaded.");
 
