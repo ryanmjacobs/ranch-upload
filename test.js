6f924484972daefe6809a6157592345bfcd07326
Ryan Jacobs <ryan@rmj.us>
2018-07-01 01:28:00 -0600

    mdrivev

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const fs       = require("fs");
 const PATH     = require("path");
 
-const FOOTAGE_DIRECTORY = PATH.join("E:", "MediaDatabase");
+const FOOTAGE_DIRECTORY = PATH.join("M:", "MediaDatabase");
 
 fs.watch(FOOTAGE_DIRECTORY, {persistent: true}, function (event, filename) {
     console.log(`${event} - ${filename}`);
