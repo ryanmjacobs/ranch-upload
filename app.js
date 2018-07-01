ab5cafaf0c6152955f987fcbb30c0b2ca0955137
Ryan Jacobs <ryan@rmj.us>
2018-07-01 12:13:31 -0600

    slash

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ async function process(path) {
     console.log("new file: " + path);
 
     mkdirp.sync(TMP_DIR);
-    const dbx_path = path.replace("pic", "mp4").replace("\\", "/");
+    const dbx_path = path.replace(/pic$/g, "mp4").replace(/\\/g, "/");
     console.log("dBX PATH", dbx_path);
     const tmp_encode = PATH.join(TMP_DIR, dbx_path);
 
