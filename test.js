f1f7ab3a0240893d76ef7ab9b9bb486a46d92fcb
Ryan Jacobs <ryan@rmj.us>
2018-07-01 01:32:45 -0600

    gaze test

--- test.js
+++ test.js
@@ -3,6 +3,36 @@ const PATH     = require("path");
 
 const FOOTAGE_DIRECTORY = PATH.join("M:", "MediaDatabase");
 
-fs.watch(FOOTAGE_DIRECTORY, {persistent: true}, function (event, filename) {
-    console.log(`${event} - ${filename}`);
+var gaze = require('gaze');
+
+// Watch all .js files/dirs in process.cwd()
+gaze("M:\\MediaDatabase\\**\\*.pic", function(err, watcher) {
+  // Files have all started watching
+  // watcher === this
+
+  // Get all watched files
+  var watched = this.watched();
+
+  // On file changed
+  this.on('changed', function(filepath) {
+    console.log(filepath + ' was changed');
+  });
+
+  // On file added
+  this.on('added', function(filepath) {
+    console.log(filepath + ' was added');
+  });
+
+  // On file deleted
+  this.on('deleted', function(filepath) {
+    console.log(filepath + ' was deleted');
+  });
+
+  // On changed/added/deleted
+  this.on('all', function(event, filepath) {
+    console.log(filepath + ' was ' + event);
+  });
+
+  // Get watched files with relative paths
+  var files = this.relative();
 });
