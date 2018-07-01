d8e123120651c8875e5db293846463206206c7dc
Ryan Jacobs <ryan@rmj.us>
2018-07-01 01:26:10 -0600

    simple test

--- test.js
+++ test.js
@@ -1,17 +1,8 @@
 const fs       = require("fs");
 const PATH     = require("path");
-const chokidar = require("chokidar");
 
 const FOOTAGE_DIRECTORY = PATH.join("E:", "MediaDatabase");
 
-READY = false;
-chokidar.watch(FOOTAGE_DIRECTORY, {ignored: /(^|[\/\\])\../})
-  .on("ready", () => {
-      READY = true;
-      console.log("ready.");
-  })
-  .on("add", path => {
-      console.log(path);
-      if (READY && path.includes("_1" + PATH.sep))
-          console.log("GOOD", path);
-  });
+fs.watch(FOOTAGE_DIRECTORY, {persistent: true}, function (event, filename) {
+    console.log(`${event} - ${filename}`);
+});
